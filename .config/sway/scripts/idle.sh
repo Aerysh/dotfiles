9e4471dfa8f24bd143ca39ab4b8c3315e2bd6e3f
Ahmad Azwar Annas <ahmadazw2@gmail.com>
2022-02-18 12:37:59 +0700

    update idle script & autostart

--- .config/sway/scripts/idle.sh
+++ .config/sway/scripts/idle.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 swayidle -w \
-	timeout 600 'swaylock -f --screenshots' \
-	timeout 900 'swaymsg "output * dpms off"' \
-  	timeout 930 'systemctl suspend' \
-	resume 'swaymsg "output * dpms on"'
-	before-sleep 'swaylock -f --screenshots'
+	timeout 300 'swaymsg "output * dpms off"' \
+	resume 'swaymsg "output * dpms on"' &
+
+swaylock
+
+kill %%
