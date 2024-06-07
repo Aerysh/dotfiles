367502135d7303d6bec70cf8bbb8e34bf797dccf
Ahmad Azwar Annas <ahmadazw2@gmail.com>
2024-06-07 23:20:58 +0700

    feat: change some executed command of power menu

--- .config/waybar/scripts/power-menu.sh
+++ .config/waybar/scripts/power-menu.sh
@@ -5,12 +5,16 @@ entries="Logout Suspend Reboot Shutdown"
 selected=$(printf '%s\n' $entries | wofi --conf=$HOME/.config/wofi/config.power --style=$HOME/.config/wofi/style.widgets.css | awk '{print tolower($1)}')
 
 case $selected in
-  logout)
-    exec loginctl kill-user $USER;;
-  suspend)
-    exec systemctl suspend;;
-  reboot)
-    exec systemctl reboot;;
-  shutdown)
-    exec systemctl poweroff -i;;
+logout)
+	exec pkill Hyprland || loginctl terminate-user $USER
+	;;
+suspend)
+	exec systemctl suspend || loginctl suspend
+	;;
+reboot)
+	exec systemctl reboot || loginctl reboot
+	;;
+shutdown)
+	exec systemctl poweroff || loginctl poweroff
+	;;
 esac
